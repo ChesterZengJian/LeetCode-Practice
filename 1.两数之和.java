390c4dc9a9bfeb7b42aeabbf58086485b797df69
Chester <374488168@qq.com>
2021-12-09 01:58:04 +0800

    add 001-twoSum

---
+++ 1.两数之和.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> numMap = new HashMap<>();
+        int[] result = new int[2];
+
+        for (int i = 0; i < nums.length; i++) {
+            if (numMap.containsKey(nums[i])) {
+                result[0] = i;
+                result[1] = numMap.get(nums[i]);
+                return result;
+            }
+
+            numMap.put(target - nums[i], i);
+        }
+
+        return result;
+    }
+}
+// @lc code=end
