7ce1a002acba6edb1236193dcef179b4eb748d79
Chester <374488168@qq.com>
2021-12-16 01:48:40 +0800

    add 020-isValid

--- 20.有效的括号.java
+++ 20.有效的括号.java
@@ -1,18 +1,12 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Stack;
+/*
+ * @lc app=leetcode.cn id=20 lang=java
+ *
+ * [20] 有效的括号
+ */
 
-import javax.xml.stream.events.StartDocument;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(isValid("{[]}"));
-    }
-
-    public static boolean isValid(String s) {
+// @lc code=start
+class Solution {
+    public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
         Map<Character, Character> map = new HashMap<>();
         map.put(')', '(');
@@ -40,3 +34,4 @@ public class Main {
         return stack.size() == 0 ? true : false;
     }
 }
+// @lc code=end
