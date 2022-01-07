0bc987dda4b08fae3aa9e33288bcd29f48566819
Chester <374488168@qq.com>
2022-01-08 00:39:19 +0800

    add 019-removeNthFromEnd

---
+++ 19.删除链表的倒数第-n-个结点.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=19 lang=java
+ *
+ * [19] 删除链表的倒数第 N 个结点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast, slow;
+        fast = slow = head;
+
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return head;
+    }
+}
+// @lc code=end
--- Main.java
+++ Main.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import java.util.Stack;
 import java.util.logging.LogManager;
 
+import javax.swing.plaf.synth.SynthButtonUI;
 import javax.xml.stream.events.StartDocument;
 
 import models.ListNode;
@@ -19,39 +20,46 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println(Integer.parseUnsignedInt("91283472332", 0, 4,
         // 10));jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-        ListNode head = new ListNode(3);
+        ListNode head = new ListNode(1);
         head.next = new ListNode(2);
-        head.next.next = head;
-        System.out.println(detectCycle(head).val);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        var result = removeNthFromEnd(head, 5);
+
+        while (result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
         // System.out.println(threeSum(new int[] { 1, 2, -2, -1 }));
     }
 
-    public static ListNode detectCycle(ListNode head) {
-        ListNode slow, fast;
-        slow = fast = head;
-        boolean hasCycle = false;
+    public static ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast, slow;
+        fast = slow = head;
 
-        while (fast != null && fast.next != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-
-            if (fast == slow) {
-                hasCycle = true;
-                break;
-            }
-        }
-
-        if (!hasCycle) {
-            return null;
-        }
-
-        fast = head;
-
-        while (fast != slow) {
-            slow = slow.next;
+        while (n > 0) {
             fast = fast.next;
+            n--;
         }
 
-        return slow;
+        System.out.println("n=" + n);
+        // System.out.println("fast=" + fast.val);
+
+        if (fast == null) {
+            head = head.next;
+            return head;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        System.out.println("n=" + n);
+        System.out.println("fast=" + fast.val);
+
+        slow.next = slow.next.next;
+        return head;
     }
 }
