30f6963a8b998fa9f40920dee072ed713a12d7b1
Chester <374488168@qq.com>
2022-01-07 23:32:50 +0800

    add 142-detectCycle

---
+++ 142.环形链表-ii.java
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=142 lang=java
+ *
+ * [142] 环形链表 II
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow, fast;
+        slow = fast = head;
+        boolean hasCycle = false;
+
+        while (fast != null && fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (fast == slow) {
+                hasCycle = true;
+                break;
+            }
+        }
+
+        if (!hasCycle) {
+            return null;
+        }
+
+        fast = head;
+
+        while (fast != slow) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow;
+    }
+}
+// @lc code=end
