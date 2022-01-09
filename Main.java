5b4912f99b9655d8bb4f2e270f5a471f8a083034
Chester <374488168@qq.com>
2022-01-09 16:11:38 +0800

    add 704-search

--- Main.java
+++ Main.java
@@ -18,21 +18,25 @@ import models.ListNode;
 public class Main {
 
     public static void main(String[] args) {
-        String str = "hello";
-        char[] chars = str.toCharArray();
-        reverseString(chars);
-        System.out.println(chars);
+        int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+        System.out.println(search(nums, 2));
     }
 
-    public static void reverseString(char[] s) {
-        int left = 0, right = s.length - 1;
+    public static int search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
 
         while (left <= right) {
-            char tmp = s[left];
-            s[left] = s[right];
-            s[right] = tmp;
-            left++;
-            right--;
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            }
         }
+
+        return -1;
     }
 }
