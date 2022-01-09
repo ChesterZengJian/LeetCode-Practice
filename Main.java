2373ecce8cf710ac49ac5e281e6f63e2bba37e5a
Chester <374488168@qq.com>
2022-01-09 15:52:32 +0800

    add 344-reverseString

--- Main.java
+++ Main.java
@@ -18,48 +18,21 @@ import models.ListNode;
 public class Main {
 
     public static void main(String[] args) {
-        // System.out.println(Integer.parseUnsignedInt("91283472332", 0, 4,
-        // 10));jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj
-        ListNode head = new ListNode(1);
-        head.next = new ListNode(2);
-        head.next.next = new ListNode(3);
-        head.next.next.next = new ListNode(4);
-        head.next.next.next.next = new ListNode(5);
-        var result = removeNthFromEnd(head, 5);
-
-        while (result != null) {
-            System.out.println(result.val);
-            result = result.next;
-        }
-        // System.out.println(threeSum(new int[] { 1, 2, -2, -1 }));
+        String str = "hello";
+        char[] chars = str.toCharArray();
+        reverseString(chars);
+        System.out.println(chars);
     }
 
-    public static ListNode removeNthFromEnd(ListNode head, int n) {
-        ListNode fast, slow;
-        fast = slow = head;
+    public static void reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
 
-        while (n > 0) {
-            fast = fast.next;
-            n--;
+        while (left <= right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = tmp;
+            left++;
+            right--;
         }
-
-        System.out.println("n=" + n);
-        // System.out.println("fast=" + fast.val);
-
-        if (fast == null) {
-            head = head.next;
-            return head;
-        }
-
-        while (fast.next != null) {
-            fast = fast.next;
-            slow = slow.next;
-        }
-
-        System.out.println("n=" + n);
-        System.out.println("fast=" + fast.val);
-
-        slow.next = slow.next.next;
-        return head;
     }
 }
