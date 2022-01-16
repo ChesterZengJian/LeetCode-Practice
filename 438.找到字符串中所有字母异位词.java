897a667db12005d266a4259e877e9ee0cf185eca
Chester <374488168@qq.com>
2022-01-17 00:23:03 +0800

    add 438-findAnagrams

--- 438.找到字符串中所有字母异位词.java
+++ 438.找到字符串中所有字母异位词.java
@@ -1,34 +1,12 @@
-import java.io.Console;
-import java.lang.reflect.Array;
-import java.time.Period;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.logging.LogManager;
+/*
+ * @lc app=leetcode.cn id=438 lang=java
+ *
+ * [438] 找到字符串中所有字母异位词
+ */
 
-import javax.management.ValueExp;
-import javax.sql.rowset.serial.SerialArray;
-import javax.swing.plaf.synth.SynthButtonUI;
-import javax.xml.stream.events.StartDocument;
-
-import models.ListNode;
-
-public class Main {
-
-    public static void main(String[] args) {
-        String s1 = "abab", s2 = "ab";
-        var result = findAnagrams(s1, s2);
-        for (Integer integer : result) {
-            System.out.print(integer + "\t");
-        }
-    }
-
-    public static List<Integer> findAnagrams(String s, String p) {
+// @lc code=start
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
         int l = 0, r = 0, valid = 0;
         Map<Character, AtomicInteger> need = new HashMap<>(),
                 window = new HashMap<>();
@@ -73,7 +51,7 @@ public class Main {
         return result;
     }
 
-    private static void incrementMap(Map<Character, AtomicInteger> map, char updatedChar) {
+    private void incrementMap(Map<Character, AtomicInteger> map, char updatedChar) {
         if (!map.containsKey(updatedChar)) {
             map.put(updatedChar, new AtomicInteger());
             return;
@@ -82,3 +60,4 @@ public class Main {
         map.get(updatedChar).incrementAndGet();
     }
 }
+// @lc code=end
