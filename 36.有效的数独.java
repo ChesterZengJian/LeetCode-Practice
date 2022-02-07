65f34828f98ccfc39825ced0f904bd963f9dbe45
Chester <374488168@qq.com>
2022-02-07 20:57:25 +0800

    adjust 36-isValidSudoku

--- 36.有效的数独.java
+++ 36.有效的数独.java
@@ -47,44 +47,5 @@ class Solution {
 
         return true;
     }
-
-    public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] == '.') {
-                    continue;
-                }
-
-                if (!isValidSudoku(board, i, j, board[i][j])) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    public boolean isValidSudoku(char[][] board, int row, int col, char c) {
-        for (int i = 0; i < board.length; i++) {
-            if (i != col && c == board[row][i]) {
-                return false;
-            }
-
-            if (i != row && c == board[i][col]) {
-                return false;
-            }
-
-            int rowIdxBoard = (row / 3) * 3 + i / 3;
-            int colIdxBoard = (col / 3) * 3 + i % 3;
-
-            if (rowIdxBoard != row && colIdxBoard != col && c == board[rowIdxBoard][colIdxBoard]) {
-                return false;
-            }
-        }
-
-        System.out.println();
-
-        return true;
-    }
 }
 // @lc code=end
