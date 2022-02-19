6f6828aec51d7edace6bd784d18f6b60a62b4cbf
Chester <374488168@qq.com>
2022-02-19 15:28:04 +0800

    optimize 45-jump

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,33 +22,47 @@ public class Main {
     }
 
     public static int jump(int[] nums) {
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, nums.length);
-        int res = dp(nums, 0, memo);
-        return res;
+        int fastest = 0, end = 0, jumps = 0;
+
+        for (int i = 0; i < nums.length-1; i++) {
+            fastest = Math.max(fastest, nums[i]+i);
+            if (i == end) {
+                jumps++;
+                end = fastest;
+            }
+        }
+
+        return jumps;
     }
 
-    public static int dp(int[] nums, int p, int[] memo) {
-        int len = nums.length;
+    // public static int jump(int[] nums) {
+    //     int[] memo = new int[nums.length];
+    //     Arrays.fill(memo, nums.length);
+    //     int res = dp(nums, 0, memo);
+    //     return res;
+    // }
 
-        // 刚好 p 为最后一步
-        if (p >= len - 1) {
-            return 0;
-        }
+    // public static int dp(int[] nums, int p, int[] memo) {
+    //     int len = nums.length;
 
-        // memo 不为初始值证明已计算过了
-        if (memo[p] != len) {
-            return memo[p];
-        }
+    //     // 刚好 p 为最后一步
+    //     if (p >= len - 1) {
+    //         return 0;
+    //     }
 
-        int steps = nums[p];
+    //     // memo 不为初始值证明已计算过了
+    //     if (memo[p] != len) {
+    //         return memo[p];
+    //     }
 
-        for (int i = 1; i <= steps; i++) {
-            int subProblem = dp(nums, p + i, memo);
-            // 子方案数+当前方案，即 sub + 1
-            memo[p] = Math.min(subProblem + 1, memo[p]);
-        }
+    //     int steps = nums[p];
 
-        return memo[p];
-    }
+    //     for (int i = 1; i <= steps; i++) {
+    //         int subProblem = dp(nums, p + i, memo);
+    //         // 子方案数+当前方案，即 sub + 1
+    //         memo[p] = Math.min(subProblem + 1, memo[p]);
+    //     }
+
+    //     return memo[p];
+    // }
 }
--- src/main/lc/45.跳跃游戏-ii.java
+++ src/main/lc/45.跳跃游戏-ii.java
@@ -7,34 +7,49 @@ package main.lc;
 
 // @lc code=start
 class Solution {
-    public int jump(int[] nums) {
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, nums.length);
-        int res = dp(nums, 0, memo);
-        return res;
+
+    public int jump(int[] nums){
+        int fastest = 0, end = 0, jumps = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            fastest = Math.max(fastest, nums[i] + i);
+            if (i == end) {
+                jumps++;
+                end = fastest;
+            }
+        }
+
+        return jumps;
     }
+    
+    // public int jump(int[] nums) {
+    //     int[] memo = new int[nums.length];
+    //     Arrays.fill(memo, nums.length);
+    //     int res = dp(nums, 0, memo);
+    //     return res;
+    // }
 
-    public int dp(int[] nums, int p, int[] memo) {
-        int len = nums.length;
+    // public int dp(int[] nums, int p, int[] memo) {
+    //     int len = nums.length;
 
-        // 刚好 p 为最后一步
-        if (p >= len - 1) {
-            return 0;
-        }
+    //     // 刚好 p 为最后一步
+    //     if (p >= len - 1) {
+    //         return 0;
+    //     }
 
-        // memo 不为初始值证明已计算过了
-        if (memo[p] != len) {
-            return memo[p];
-        }
+    //     // memo 不为初始值证明已计算过了
+    //     if (memo[p] != len) {
+    //         return memo[p];
+    //     }
 
-        int steps = nums[p];
+    //     int steps = nums[p];
 
-        for (int i = 1; i <= steps; i++) {
-            int subProblem = dp(nums, p + i, memo);
-            memo[p] = Math.min(subProblem + 1, memo[p]);
-        }
+    //     for (int i = 1; i <= steps; i++) {
+    //         int subProblem = dp(nums, p + i, memo);
+    //         memo[p] = Math.min(subProblem + 1, memo[p]);
+    //     }
 
-        return memo[p];
-    }
+    //     return memo[p];
+    // }
 }
 // @lc code=end
